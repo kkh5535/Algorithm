8c6935d8210454a42ae48517484d684798656673
강경훈 <kkh5535@naver.com>
2024-08-03 01:12:56 +0900

    [level 0] Title: 2의 영역, Time: 1.60 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        int start = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+        if (start == -1) {
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int end = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+        
+        int[] answer = new int[end - start + 1];
+        for (int i = start; i <= end; i++) {
+            answer[i - start] = arr[i];
+        }
+                
+        return answer;
+    }
+}
