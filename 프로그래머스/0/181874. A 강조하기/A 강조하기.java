63b4dd70fb15e7c4ae11ed5170a0658e15730827
강경훈 <kkh5535@naver.com>
2024-08-05 22:52:55 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toLowerCase();
+        return myString.replace("a", "A");
+    }
+}
