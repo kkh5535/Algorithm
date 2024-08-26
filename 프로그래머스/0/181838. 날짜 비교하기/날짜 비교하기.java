5a89587be7f9b0349c8373590397bbd553181a4c
강경훈 <kkh5535@naver.com>
2024-08-26 17:11:53 +0900

    [level 0] Title: 날짜 비교하기, Time: 8.13 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        Date d1 = new Date(date1[0], date1[1], date1[2]);
+        Date d2 = new Date(date2[0], date2[1], date2[2]);
+        return d1.before(d2) ? 1 : 0;
+    }
+}
